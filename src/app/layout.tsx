f14bcc59824095f500665bd6064cd750cf1d64f6
PratikC22 <pratik.chaudhari@nference.net>
2025-07-13 05:37:29 +0530

    feat: added analytics support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/next'
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
@@ -47,6 +48,7 @@ export default function RootLayout({
           <main className='flex-1'>{children}</main>
           <Footer />
           <ConsoleCredit />
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
